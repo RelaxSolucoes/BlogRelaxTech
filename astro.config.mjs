00d0d5ce47c4f290ddc3b1138e086818cb89aee0
Relax Soluções <chatrelaxbr@gmail.com>
2025-05-29 19:27:51 -0300

    Fix Netlify Deployment Configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://blog-relax-tech.netlify.app',
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
+  adapter: netlify({
+    edgeMiddleware: true
   }),
   integrations: [
     tailwind(),
