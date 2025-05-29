88380ffe728139a357241b78198ca8361ce874c6
Relax Soluções <chatrelaxbr@gmail.com>
2025-05-29 19:15:20 -0300

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://seu-blog.com.br',
   output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [
     tailwind(),
     sitemap(),
